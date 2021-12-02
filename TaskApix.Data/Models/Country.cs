bf0b55fc1b4b3f92867b935c5c8e4f4c26210b2f
Jasurbek Khasanboyev <81855769+jasurbuz@users.noreply.github.com>
2021-12-02 11:12:24 +0500

    Add project files.

---
+++ TaskApix.Data/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApix.Data.Models
+{
+    public class Country
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Code { get; set; }
+        public bool IsSchengen { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+
+        #region
+        public ICollection<Region> Regions { get; set; }
+        #endregion
+    }
+}
---
+++ TaskApix.Data/Models/Region.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApix.Data.Models
+{
+    public class Region
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+
+        #region Relation
+        public long CountryId { get; set; }
+        public Country Country { get; set; }
+        #endregion
+    }
+}
